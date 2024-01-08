5579142a3fd842f998aec9f30b96749441c5bf7b
simochee <lollipop@simochee.net>
2024-01-08 22:13:18 +0900

    chore: rename placeholder

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,9 @@
-import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
 
-export default defineConfig(
-  [true, false].map(
-    (minify) =>
-      /** @type {import('rollup').RollupOptions} */
-      ({
-        input: "src/index.js",
-        output: {
-          file: `dist/bundle${minify ? ".min.js" : ".js"}`,
-          format: "umd",
-        },
-        plugins: [minify ? terser() : undefined],
-      }),
-  ),
-);
+export default defineConfig({
+  input: "src/index.js",
+  output: {
+    file: process.env.npm_package_main,
+    format: "umd",
+  },
+});
